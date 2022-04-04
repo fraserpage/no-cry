5f4db8894d41036a06cb6e6d353648fa728b4643
Fraser Page <fraser.page@briteweb.com>
2022-04-04 16:19:47 -0400

    handle branch already exists

--- app/Commands/Concerns/InteractsWithGit.php
+++ app/Commands/Concerns/InteractsWithGit.php
@@ -12,8 +12,23 @@ trait InteractsWithGit
         $now = Carbon::now();
         $branchName = "{$ticket}-plugin-updates-{$now->format('Y-m-d')}";
 
-        exec("git checkout -b {$branchName}");
+        exec("git checkout -b {$branchName} 2>&1", $output, $result);
+
+        if ($result === 128){
+            $this->branchAlreadyExists($branchName);
+        }
 
         return $branchName;
+    }
+
+    private function branchAlreadyExists($branchName){
+        if ($this->confirm("A branch named ".$branchName." already exists. Do you want to use it?", true)){
+            exec("git checkout {$branchName}");
+        }
+        else{
+            $now = Carbon::now();
+            $branchPrefix = $this->ask("Ok, lets start over. Let's make a new branch named [WHATEVER]-plugin-updates-{$now->format('Y-m-d')}. What would you like [WHATEVER] to be?");
+            $this->checkoutNewBranchForDate($branchPrefix);
+        }
     }
 }
