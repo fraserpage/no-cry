2165e2d7b3f5959e7644b4fab54295dadb3d7363
Fraser Page <fraser.page@briteweb.com>
2022-05-19 14:44:39 -0400

    only ask to update wp core if there's an update

--- app/Commands/Concerns/InteractsWithWpCore.php
+++ app/Commands/Concerns/InteractsWithWpCore.php
@@ -6,32 +6,34 @@ trait InteractsWithWpCore
 {
     public function updateWPCore($lando): string
     {
-        if ($this->confirm("Want to update WP Core?",true)){
-            
-            $result = '';
-            exec("{$lando} wp core check-update --format=json 2>&1", $updateCheck);
+           
+        $result = '';
+        exec("{$lando} wp core check-update --format=json 2>&1", $updateCheck);
 
-            if (count($updateCheck)){
-                $this->line("WordPress update avialable.");
+        if (count($updateCheck)){
+            if ($this->confirm("There's a WordPress update avialable. Install it?",true)){
+
                 exec("{$lando} wp core version 2>&1", $currentVersion, $getVersionResult);
 
                 $this->line("Updating Wordpress...");
                 exec("{$lando} wp core update 2>&1", $updateVersion, $updateResult);
                 exec("{$lando} wp core version 2>&1", $newVersion, $getNewVersionResult);
                 
+                var_dump($currentVersion);
                 $result = "Wordpress from $currentVersion[0] to $newVersion[0]";
                 $this->line($result);
 
                 exec('git add -A');
                 exec("git commit -m 'deps(wp-core): {$result}'");
+
+                return $result;
             }
             else{
-                $this->line("WordPress is at the latest version.");
+                return '';
             }
-
-            return $result;
         }
         else{
+            $this->line("WordPress is at the latest version.");
             return '';
         }
     }
